c4530bede37809d3c43700f9ba48fc15a6f1a94d
Aaron Hillegass <aaron@bignerdranch.com>
2020-10-23 18:46:13 -0400

    Changed minkpoint.sh to use conda

--- admin/minkpoint.sh
+++ admin/minkpoint.sh
@@ -6,9 +6,10 @@
 apt-get -y update
 apt-get -y upgrade
 
-# Install standard dev tools
-apt-get -y install gcc git emacs-nox
-apt-get -y install python3 python3-pip curl cmake libboost-all-dev
+# Install standard dev tools, llvm10 is needed by numba
+apt-get -y install gcc git emacs-nox llvm-10-dev
+apt-get -y install python3.6 python3-pip curl cmake libboost-all-dev 
+ln -s /usr/bin/llvm-config-10 /usr/bin/llvm-config
 
 # Install CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
@@ -21,34 +22,48 @@ apt-get -y install --no-install-recommends cuda
 
 # Install machine learning stuff
 apt-get -y install libopenblas-dev
-pip3 install --no-input numpy mkl-include torch torchvision
 
-# Make sure everything is using python 3 before trying to build MinkowskiEngine
-ln -sf /usr/bin/python3 /usr/bin/python
+# Install conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod a+x Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
 
-pip3 install --no-input nuscenes-devkit
+conda create --name minkpoint python=3.6
+conda activate minkpoint
+conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.2 -c pytorch
 
-pip3 install --no-input -U MinkowskiEngine --install-option="--blas=openblas" -v
+pip3 install scikit-build
+pip3 install numpy
 
-git clone https://github.com/NVIDIA/MinkowskiEngine.git
-cd MinkowskiEngine
-python3 setup.py install --blas=openblas
-cd ..
+# Install nuScenes developer kit
+git clone https://github.com/tianweiy/nuscenes-devkit
 
 # Install apex, spconv for CenterPoint
 git clone https://github.com/NVIDIA/apex
 cd apex
 git checkout 5633f6  # recent commit doesn't build in our system 
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+pip3 install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 cd ..
 
+# Install spconv
 git clone https://github.com/traveller59/spconv.git --recursive
 cd spconv && git checkout 7342772
 python3 setup.py bdist_wheel
 cd ./dist && pip install *
-cd ../..
+cd ..
 
+# Install CenterPoint
 git clone https://github.com/tianweiy/CenterPoint.git
 cd CenterPoint
 pip3 install -r requirements.txt
+
+# pip3 install --no-input -U MinkowskiEngine --install-option="--blas=openblas" -v
+git clone https://github.com/NVIDIA/MinkowskiEngine.git
+cd MinkowskiEngine
+python3 setup.py install --blas=openblas
+cd ..
+
+chown -R charmedliferaft ~
+
+echo "Add CenterPoint and nuscenes/python-sdk to your PYTHONPATH env var."
 
