63de1753af09087b1c9554d0b666f61565c17f5e
Aaron Hillegass <aaron@bignerdranch.com>
2020-10-25 23:58:17 -0400

    Little changes...

--- admin/minkpoint2.sh
+++ admin/minkpoint2.sh
@@ -6,6 +6,21 @@ apt-get -y install libopenblas-dev
 apt install ./libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
 apt install ./libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
 
+# Fetch nuScenes developer kit
+git clone https://github.com/tianweiy/nuscenes-devkit
+
+# Fetch apex
+git clone https://github.com/NVIDIA/apex
+
+# Fetch spconv
+git clone https://github.com/traveller59/spconv.git --recursive
+
+# Fetch CenterPoint
+git clone https://github.com/tianweiy/CenterPoint.git
+
+# Fetch MinkowskiEngine
+git clone https://github.com/NVIDIA/MinkowskiEngine.git
+
 # Install conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
@@ -14,3 +29,4 @@ bash Miniconda3-latest-Linux-x86_64.sh
 echo "RUN THESE"
 echo "conda create -y --name minkpoint python=3.6"
 echo "conda activate minkpoint"
+echo "comment the check for cuda version in apex/setup.py"
--- admin/minkpoint3.sh
+++ admin/minkpoint3.sh
@@ -14,42 +14,33 @@ cd /home/charmedliferaft
 
 # Use conda to install some stuff
 conda install -y -c conda-forge scikit-build
-conda install -y pytorch==1.1.0 torchvision==0.3.0 cudatoolkit=10.0 -c pytorch
+conda install -y pytorch==1.5.0 torchvision==0.3.0 cudatoolkit=10.2 -c pytorch
 conda install -y tensorflow
 
 export CUDACXX="/usr/local/cuda/bin/nvcc"
 
-# Install nuScenes developer kit
-git clone https://github.com/tianweiy/nuscenes-devkit
-
-# Install apex, spconv for CenterPoint
-git clone https://github.com/NVIDIA/apex
 cd apex
 git checkout 5633f6  # recent commit doesn't build in our system 
 pip3 install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-cd ..
+cd ~
 
-# Install spconv
-git clone https://github.com/traveller59/spconv.git --recursive
 # This version is recommended, but doesn't build
 # cd spconv && git checkout 7342772
 cd spconv
 python3 setup.py bdist_wheel
 cd dist
 pipe install *
-cd ../..
+cd ~
 
-# Install CenterPoint
-git clone https://github.com/tianweiy/CenterPoint.git
 cd CenterPoint
 pip3 install -r requirements.txt
 mkdir work_dirs
 mkdir work_dirs/centerpoint_pillar_512_demo
-tar xvzf ../demo.tgz
+tar xzf ../demo.tgz
 cp ../last.pth work_dirs/centerpoint_pillar_512_demo
+cd ~
 
 # pip3 install --no-input -U MinkowskiEngine --install-option="--blas=openblas" -v
-git clone https://github.com/NVIDIA/MinkowskiEngine.git
 cd MinkowskiEngine
 python3 setup.py install
 cd ..
